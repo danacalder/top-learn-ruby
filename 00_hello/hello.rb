7c9ec31c440fa9aad772fd2c5ef0b5986f012dc1
Dana Calder <dana.calder@gmail.com>
2018-10-03 00:17:58 +0000

    practice

--- 00_hello/hello.rb
+++ 00_hello/hello.rb
@@ -1,1 +1,19 @@
-#write your code here
+def hello(name) #naming method with def and closing with end and pass argument with name
+    if name == "Kristine" #== is a boolean
+        puts "Hello #{name}"
+    elseif name == "Romina"
+        puts "Hello Romina"
+    else
+    puts "Hello #{name}"
+    end
+end
+
+def main
+    print "What is your name? "
+    name = gets.chomp #get user input
+    hello(name)
+end
+
+#hello () #calling your method
+
+main if __FILE__ ==$PROGRAM_NAME
--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,1 +1,13 @@
 #write your code here
+def ftoc(temp)
+    T(°C) = (T(°F) - 32) x 5/9
+    temp = (temp - 32) * (5.0/9)
+    temp.round (2)
+end
+
+def ctof(temp)
+    T(°F) = (T(°C) + 32) * 1.8
+    temp = (temp * 1.8) + 32
+end
+
+main if __FILE__ ==$PROGRAM_NAME
