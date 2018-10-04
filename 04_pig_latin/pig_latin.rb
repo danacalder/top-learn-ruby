d35aa3a768aa1a8641655f1e7343999d3721858e
Dana Calder <dana.calder@gmail.com>
2018-10-04 23:47:11 +0000

    trying again

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,15 +1,16 @@
 def pig_latin(word)
-    word_vowel = word.split ("")
-    word_vowel.each do |x|
+    word = word.split ("")
+    word.each do |x|
         if ["a", "e", "i", "o", "u"].include?x[0,1]
-        else put word_vowel = word + "ay"
+        else put word = word + "ay"
         end 
 end 
 
 def main
     print "What is your name?"
     word = gets.chomp
-    hello (word)
-end
+   pig_latin(word)
+endgit 
 
+end 
 main if __FILE__ ==$PROGRAM_NAME
