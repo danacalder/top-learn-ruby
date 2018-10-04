8b90636e3ed4f130b63cf169f196b1fe8d0de927
Dana Calder <dana.calder@gmail.com>
2018-10-04 23:40:31 +0000

    trying to call method

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,9 +1,15 @@
-def pig_latin (word)
+def pig_latin(word)
     word_vowel = word.split ("")
     word_vowel.each do |x|
         if ["a", "e", "i", "o", "u"].include?x[0,1]
-        else put word_vowel = word + 'ay'
+        else put word_vowel = word + "ay"
         end 
 end 
+
+def main
+    print "What is your name?"
+    word = gets.chomp
+    hello (word)
+end
 
 main if __FILE__ ==$PROGRAM_NAME
