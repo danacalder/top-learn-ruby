e1ae9cf7f507dfcbd857fd04a5dfc4a1d13ce646
Dana Calder <dana.calder@gmail.com>
2018-10-04 23:33:50 +0000

    pig latin exercise

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,9 @@
-#write your code here
+def pig_latin (word)
+    word_vowel = word.split ("")
+    word_vowel.each do |x|
+        if ["a", "e", "i", "o", "u"].include?x[0,1]
+        else put word_vowel = word + 'ay'
+        end 
+end 
+
+main if __FILE__ ==$PROGRAM_NAME
