38c6c0b8ba710dbca911d364248b932753ebbaa0
Dana Calder <dana.calder@gmail.com>
2018-10-05 00:34:38 +0000

    more practice

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -3,8 +3,8 @@ def pig_latin(word)
     # word.each do |x|
         if ["a", "e", "i", "o", "u"].include?(word[0])
             puts word + "ay"
-        # elseif ["b","c","d","f","g","h","j","k","l","m","n","p","q","r","s","t","v","w","x,"y","z"].reverse.chop.reverse
-        #     puts word + "ay"
+        # elseif word.slice! #trying to remove consonent argh
+            puts word + "ay"
           else 
             puts word + word[0] + "ay"  
         end 
