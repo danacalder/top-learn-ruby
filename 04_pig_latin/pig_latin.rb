3a8b1a3f5d85aaa0579659f909767a10e77d0b57
Dana Calder <dana.calder@gmail.com>
2018-10-05 00:19:02 +0000

    stuck but further

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,8 +1,12 @@
 def pig_latin(word)
-    word = word.split ("")
-    word.each do |x|
-        if ["a", "e", "i", "o", "u"].include?x[0,1]
-        else put word = word + "ay"
+    # word = word.split ("")
+    # word.each do |x|
+        if ["a", "e", "i", "o", "u"].include?(word[0])
+            puts word + "ay"
+        # elseif ["b","c","d","f","g","h","j","k","l","m","n","p","q","r","s","t","v","w","x,"y","z"].reverse.chop.reverse
+        #     puts word + "ay"
+          else 
+            puts word + word[0] + "ay"  
         end 
 end 
 
@@ -10,7 +14,6 @@ def main
     print "What is your name?"
     word = gets.chomp
    pig_latin(word)
-endgit 
-
 end 
+
 main if __FILE__ ==$PROGRAM_NAME
